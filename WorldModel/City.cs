7cba42d1999a27b1e02c3713077b6b9477d399b5
Chen <connie.chen.490@my.csun.edu>
2025-09-26 18:10:01 -0700

    Database scaffolding added

---
+++ WorldModel/City.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace WorldModel;
+
+[Table("City")]
+public partial class City
+{
+    [Key]
+    [Column("ID")]
+    public int Id { get; set; }
+
+    [Column("countryID")]
+    public int CountryId { get; set; }
+
+    [Column("latitude")]
+    public double Latitude { get; set; }
+
+    [Column("longitude")]
+    public double Longitude { get; set; }
+
+    [Column("population")]
+    public int Population { get; set; }
+
+    [ForeignKey("CountryId")]
+    [InverseProperty("Cities")]
+    public virtual Country Country { get; set; } = null!;
+}
